ac104e172f998019b83bbf193b9062a011e96907
Daewooo91 <youpdw131@gmail.com>
2022-09-15 12:48:35 +0900

    JS: remove active from nav

--- main.js
+++ main.js
@@ -59,6 +59,12 @@ workBtnContainer.addEventListener('click', (e) => {
   if(filter == null) {
     return;
   }
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
+
   projectContainer.classList.add('anim-out')
   setTimeout(() => {
     projects.forEach((project) => {
@@ -69,7 +75,6 @@ workBtnContainer.addEventListener('click', (e) => {
         project.classList.add('invisible');
       }
     });
-
     projectContainer.classList.remove('anim-out')
   }, 300);
 });
