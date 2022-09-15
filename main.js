db1e5953365bf4d655b534fc5c9cde25d13d2219
Daewooo91 <youpdw131@gmail.com>
2022-09-15 12:31:45 +0900

    JS: Project filtering

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector('#navbar ');
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   if(window.scrollY > navbarHeight) {
@@ -46,9 +46,34 @@ document.addEventListener('scroll', () => {
 });
 
 // Handle click on the "arrow-up" button
-document.addEventListener('click', () => {
+arrowUp.addEventListener('click', () => {
   scrollIntoView('#home')
 });
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if(filter == null) {
+    return;
+  }
+  projectContainer.classList.add('anim-out')
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if(filter == '*' || filter == project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+
+    projectContainer.classList.remove('anim-out')
+  }, 300);
+});
+
 
 
 function scrollIntoView(selector) {
